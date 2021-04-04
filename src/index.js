04691bb8f5b553bad301d070ea4cf72c5c5e9372
Tim Carver <tfc13b@gmail.com>
2021-04-04 13:01:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 //create a react componet
 const App = () =>{
-    return <div> Hi there</div>
+    const getButtonText = {text: 'Click ME!'}
+    const labelText = 'Enter Name:'
+   return ( <div>
+   <label htmlFor="Name" className="label">{labelText}</label>
+   <input id="Name" type="text" />
+   <button style={{backgroundColor:'blue', color:'white'}}>{getButtonText.text}</button>
+ </div>
+ );
 };
 
 //Take the react componet and show it on the screen
